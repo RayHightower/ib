210ef6f51c080886d7d4f4f3f91fdffa68a2ec8f
Raymond T. Hightower <rth@wisdomgroup.com>
2012-08-14 13:15:47 -0500

    interface builder works

--- home_controller.rb
+++ home_controller.rb
@@ -3,6 +3,12 @@ class HomeController < UIViewController
     def loadView
       views = NSBundle.mainBundle.loadNibNamed "View", owner:self, options:nil
       self.view = views[0]
+    end
+
+    def viewDidLoad
+      button = view.viewWithTag 1
+      @label = view.viewWithTag 2
+      button.addTarget(self, action:'buttonTapped:', forControlEvents:UIControlEventTouchUpInside)
       
     end
 end
